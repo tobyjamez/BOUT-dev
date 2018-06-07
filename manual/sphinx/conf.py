1a5f25fa15734aceb966627e19950240c5d599dc
David Schwörer <schword2@mail.dcu.ie>
2018-06-07 14:33:59 +0100

    add favicon

--- manual/sphinx/conf.py
+++ manual/sphinx/conf.py
@@ -190,6 +190,12 @@ else:
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# If given, this must be the name of an image file (path relative to
+# the configuration directory) that is the favicon of the docs. Modern
+# browsers use this as the icon for tabs, windows and bookmarks. It
+# should be a Windows-style icon file (.ico), which is 16x16 or 32x32
+# pixels large. Default: None.
+html_favicon = ['favicon.ico']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
